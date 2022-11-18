039ce823a4492be332f9335af93bf8e450a6913f
Roman Kelesidis <roman25052006.kelesh@Gmail.com>
2022-11-18 17:34:11 +0700

    Fixed in_array comparing for $server_port

--- common.php
+++ common.php
@@ -61,7 +61,7 @@ if (file_exists(__DIR__ . '/library/config.local.php')) {
 
 // Server
 $server_protocol = $bb_cfg['server_secure'] ? 'https://' : 'http://';
-$server_port = in_array($bb_cfg['server_port'], [80, 443], true) ? '' : (':' . $bb_cfg['server_port']);
+$server_port = in_array($bb_cfg['server_port'], [80, 443]) ? '' : (':' . $bb_cfg['server_port']);
 define('FORUM_PATH', $bb_cfg['script_path']);
 define('FULL_URL', $server_protocol . $bb_cfg['server_name'] . $server_port . $bb_cfg['script_path']);
 unset($server_protocol, $server_port);
